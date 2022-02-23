2241894d56cb07aaa6e7fa21e723181d2bc1b8d8
Kantemir <mr.kant2001@mail.ru>
2022-02-24 01:21:31 +0300

    исправление header footer

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+
 const swiper = new Swiper('.swiper', {
   speed: 400,
   spaceBetween: 0,
@@ -12,7 +13,7 @@ const swiper = new Swiper('.swiper', {
     clickable: true,
   },
 });
-
+/*
 var tabNavs = document.querySelectorAll(".nav-tab");
 var tabPanes = document.querySelectorAll(".tab-pane");
 
@@ -39,26 +40,43 @@ for (var i = 0; i < tabNavs.length; i++) {
 let popupBg = document.querySelector('.popup-bg');
 let popup = document.querySelector('.popup');
 let openPopupButtons = document.querySelectorAll('.open-popup');
-let closePopupButton = document.querySelector('.close-popup'); 
+let closePopupButton = document.querySelector('.close-popup');
 
 
-openPopupButtons.forEach((button) => { 
-  button.addEventListener('click', (e) => { 
-      e.preventDefault();
-      popupBg.classList.add('active'); 
-      popup.classList.add('active');
-      document.body.style.overflow = 'hidden'; 
+openPopupButtons.forEach((button) => {
+  button.addEventListener('click', (e) => {
+    e.preventDefault();
+    popupBg.classList.add('active');
+    popup.classList.add('active');
+    document.body.style.overflow = 'hidden';
   })
 });
-closePopupButton.addEventListener('click',() => { 
-  popupBg.classList.remove('active'); 
-  popup.classList.remove('active');
-  document.body.style.overflow = 'visible'; 
+closePopupButton.addEventListener('click', () => {
+  popupBg.classList.remove('active');
+  document.body.style.overflow = 'visible';
 });
-document.addEventListener('click', (e) => { 
-  if(e.target === popupBg) { 
-      popupBg.classList.remove('active'); 
-      popup.classList.remove('active');
-      document.body.style.overflow = 'visible'; 
+document.addEventListener('click', (e) => {
+  if (e.target === popupBg) {
+    popupBg.classList.remove('active');
+    document.body.style.overflow = 'visible';
   }
-});
+});*/
+
+
+let navBg = document.querySelector('.nav__wrapper');
+let nav = document.querySelector('.nav__list');
+let openNav = document.querySelector('.nav');
+let closeNav = document.querySelector('.close-nav');
+
+openNav.addEventListener('click', ()=> {
+  navBg.classList.add('active');
+  document.body.style.overflow = 'hidden';
+})
+
+document.addEventListener('click', (event)=> {
+  console.log(event.target);
+  if ((event.target  ==  closeNav) || (event.target  ==  navBg)) {
+    navBg.classList.remove('active');
+    document.body.style.overflow = 'visible';
+  }
+})
